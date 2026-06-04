e70f8c3f99a4d26aba50f0d432ccfa050855da80
aalzriqat <aalzriqat@gmail.com>
2026-06-04 09:52:35 +0300

    fix: next config format

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // In Next.js 15, we define an array of IPs or hostnames allowed for HMR
-  allowedDevOrigins: ["192.168.1.17", "localhost:3000", "0.0.0.0"],
+  allowedDevOrigins: ["http://192.168.1.17"],
 };
 
 export default nextConfig;
